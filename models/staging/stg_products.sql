25c754d6b8daf949646b0ca16d1a2007e6665587
Rodgers <roggybunde@gmail.com>
2026-05-08 20:47:54 +0300

    initial commit

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -12,9 +12,9 @@ from (
                order by product_id
            ) as rn
     from {{ ref('products') }}
-	where product_id is not null
-	 and product_name is not null
-        and category is not null
-        and price is not null
+    where nullif(trim(product_id::text), '') is not null
+      and nullif(trim(product_name), '') is not null
+      and nullif(trim(category), '') is not null
+      and price is not null
 ) t
 where rn = 1
